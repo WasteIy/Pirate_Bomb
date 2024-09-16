f11684f7a5befe03f3d50184d0a7603fc6f70522
João Lucas Silva <joaolucas.silva@estudante.ufjf.br>
2024-09-16 14:44:24 -0300

    Implement bomb spawning and sprites

---
+++ scenes/bomb.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+@onready var sprite = $AnimatedSprite2D
+
+func _on_timer_timeout() -> void:
+	sprite.animation = "explosion"
+	if sprite.animation == "explosion":
+		await sprite.animation_finished
+		queue_free()
+
+#TODO Fix rotation
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,17 @@
 extends CharacterBody2D
 @onready var sprite = $AnimatedSprite2D
 
-
 var SPEED = 130.0
-const JUMP_VELOCITY = -260.0
+const JUMP_VELOCITY = -400.0
+
+var bomb_scene = preload("res://scenes/bomb.tscn")
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	var direction = Input.get_axis("left", "right")
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -23,16 +21,28 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		if direction == 0:
-			sprite.play("idle")
+			if sprite.animation != "idle":
+				sprite.play("idle")
 		else:
-			sprite.play("run")
+			if sprite.animation != "run":
+				sprite.play("run")
 	else:
 		if velocity.y < 0:
-			sprite.play("jump")
+			if sprite.animation != "jump":
+				sprite.play("jump")
 		else:
-			sprite.play("fall")
-
+			if sprite.animation != "fall":
+				sprite.play("fall")
 	if direction > 0:
 		sprite.flip_h = false
 	elif direction < 0:
 		sprite.flip_h = true
+
+func _process(delta):
+	if Input.is_action_just_released("bomb"):
+		throw_projectile()
+
+func throw_projectile():
+	var bomb_scene = bomb_scene.instantiate()
+	bomb_scene.position = global_position
+	get_parent().add_child(bomb_scene)
