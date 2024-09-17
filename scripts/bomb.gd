b050da47ad96ce2d91f8c3eb89a08a8445e17879
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2024-09-17 15:34:03 -0300

    Implement enemy moving towards the bomb

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 @onready var sprite = $AnimatedSprite2D
 
 func _on_timer_timeout() -> void:
-	sprite.animation = "explosion"
+	sprite.play("explosion")
 	if sprite.animation == "explosion":
 		await sprite.animation_finished
 		queue_free()
--- scripts/cucumber.gd
+++ scripts/cucumber.gd
@@ -1,38 +1,51 @@
 extends CharacterBody2D
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var ray_cast_left: RayCast2D = $RayCast2DLeft
-@onready var ray_cast_right: RayCast2D = $RayCast2DRight
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var area: Area2D = $Area2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-@onready var direction = -1
+const THRESHOLD = 40
+var bomb : RigidBody2D = null
+
+var direction = 0
+var bomb_position: Vector2 = Vector2.ZERO
+var has_target: bool = false
 
 func _physics_process(delta: float) -> void:
-	
-	if ray_cast_right.is_colliding():
-		direction = -1
-	if ray_cast_left.is_colliding():
-		direction = 1
-		
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-	if direction:
-		velocity.x = direction * SPEED
+	if has_target:
+		move_towards_bomb(delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
 	
-	if is_on_floor():
-		if direction == 0:
-			if sprite.animation != "idle":
-				sprite.play("idle")
+	move_and_slide()
+	update_animation()
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Bomb":
+		bomb = body
+		bomb_position = bomb.position
+		has_target = true
+
+func move_towards_bomb(delta: float) -> void:
+	var direction_bomb = (bomb_position - position).normalized()
+	velocity.x = direction_bomb.x * SPEED
+	
+	if position.distance_to(bomb_position) < THRESHOLD:
+		has_target = false
+		velocity = Vector2.ZERO
+		
+		#TODO Update animation depending on the side the bomb is
+		sprite.play("blow")
+		
+func update_animation():
+		if abs(velocity.x) > 0:
+			sprite.play("run")
+		if velocity.x > 0:
+			sprite.flip_h = true
 		else:
-			if sprite.animation != "run":
-				sprite.play("run")
-	if direction > 0:
-		sprite.flip_h = true
-	elif direction < 0:
-		sprite.flip_h = false
+			sprite.flip_h = false
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "blow":
+		sprite.play("idle")
