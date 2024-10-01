7cf71723e39fb626593215f90b66a49d89c1d72b
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2024-10-01 18:25:27 -0300

    Start bald pirate implementation

--- scripts/bald_pirate.gd
+++ scripts/bald_pirate.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @onready var ray_cast_left: RayCast2D = $Node2D/RayCast2DLeft
 @onready var ray_cast_right: RayCast2D = $Node2D/RayCast2DRight
 
-const SPEED = 100.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 const THRESHOLD = 40
 
@@ -17,6 +17,7 @@ var dead: bool = false
 var friction = 20
 var exploded = false
 var health = 2
+var knockback = Vector2(200, -400)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -32,24 +33,26 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	update_animation()
 	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if has_target and is_instance_valid(bomb):
+func _on_vision_body_entered(body: Node2D) -> void:
+	if has_target and is_instance_valid(body):
 		if abs(body.global_position.x - global_position.x) > abs(bomb.global_position.x - global_position.x):
 			return
 		else:
 			pass
+	if body != bomb:
+		has_target = true
 	bomb = body
 	bomb_position = bomb.position
-	has_target = true
 
 func move_towards_bomb(_delta: float) -> void:
-	direction = sign(bomb_position.x - position.x)
-	velocity.x = direction * SPEED
+	if has_target:
+		direction = sign(bomb_position.x - position.x)
+		velocity.x = direction * SPEED
 
 	if abs(bomb_position.x - position.x) < THRESHOLD:
 		velocity = Vector2.ZERO
 		if has_target:
-			blow_fuse()
+			kick_bomb()
 
 func update_animation():
 	if health > 0 and !exploded:
@@ -58,7 +61,7 @@ func update_animation():
 					sprite.play("fall")
 			elif velocity.x != 0:
 				sprite.play("run")
-			elif sprite.animation != "blow": 
+			elif sprite.animation != "kick": 
 				sprite.play("idle")
 	elif exploded:
 		if health > 0:
@@ -75,21 +78,27 @@ func update_animation():
 			sprite.play("dead")
 	
 	if direction > 0:
-		sprite.flip_h = true
-	elif direction < 0:
 		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
 	
 	if ray_cast_right.is_colliding():
-			sprite.flip_h = true
-	if ray_cast_left.is_colliding():
 			sprite.flip_h = false
+	if ray_cast_left.is_colliding():
+			sprite.flip_h = true
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if sprite.animation == "blow":
-		bomb.fuse = false
+	if sprite.animation == "kick":
 		sprite.play("idle")
 
-func blow_fuse():
+func kick_bomb():
 	if ray_cast_left.is_colliding() or ray_cast_right.is_colliding():
-		sprite.play("blow")
+		sprite.play("kick")
+		apply_knockback(bomb)
 		has_target = false
+
+func apply_knockback(body: RigidBody2D):
+	direction = sign(body.global_position.x - global_position.x)
+	body.linear_velocity.x = knockback.x * direction
+	body.linear_velocity.y = knockback.y
+	body.exploded = true
